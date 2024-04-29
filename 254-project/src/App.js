d26d9a23aceea6e0249082c250deee6dc5366e65
Jacob <silverlock99@gmail.com>
2024-04-29 01:15:32 -0700

    Add basic tests

--- 254-project/src/App.js
+++ 254-project/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
+const testList = [
+  { title: 'item1', id: 1},
+  {title: 'item2', id: 2},
+  {title: 'item3', id: 3},
+]
 
+const listItems = testList.map(item =>
+  <li key={item.id}>
+    {item.title}
+  </li>
+)
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>CPSC 254 Project</h1>
+
+        <button>Click here to get started!</button>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <ul>{listItems}</ul>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
