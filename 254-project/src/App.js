591dbb22246303aab420c82594320584786773b7
Jacob <silverlock99@gmail.com>
2024-04-30 13:44:00 -0700

    Install express server

--- 254-project/src/App.js
+++ 254-project/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import axios from 'axios';
 
 const testList = [
   { title: 'item1', id: 1},
   {title: 'item2', id: 2},
   {title: 'item3', id: 3},
 ]
+
+const apiCall = () => {
+  axios.get('https://localhost:8080').then((data) => {
+    console.log(data)
+  })
+}
 
 const listItems = testList.map(item =>
   <li key={item.id}>
@@ -18,7 +25,7 @@ function App() {
       <header className="App-header">
         <h1>CPSC 254 Project</h1>
 
-        <button>Click here to get started!</button>
+        <button onClick={apiCall}>Click here to make API Call</button>
         <p>
           <ul>{listItems}</ul>
         </p>
