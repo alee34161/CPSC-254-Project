72a3d1c15107ac5f08d6b82f9bcc82051d671939
Jacob <silverlock99@gmail.com>
2024-05-02 11:43:19 -0700

    Set up express server calls

--- 254-project/src/App.js
+++ 254-project/src/App.js
@@ -1,20 +1,10 @@
 import './App.css';
 import axios from 'axios';
 import moment from 'moment';
-import { useState } from 'react';
-import { Table, TableHead, TableBody, TableCell, TableRow, TableContainer, Paper, ThemeProvider } from '@mui/material';
-import { createTheme } from '@mui/material/styles';
 
 const weatherKey = '01d6a2b501df4c29a61224906242304';
 
-const theme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
-
 function App() {
-  const [weatherData, setWeatherData] = useState(null);
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -32,14 +22,23 @@ function App() {
     axios
       .get(url)
       .then((response) => {
-        console.log("Location: " + response.data.location.name + ", " + response.data.location.region + ", " + response.data.location.country)
-        console.log("Temp: " + response.data.current.temp_f + "°F");
-        console.log("Local time: " + moment(response.data.location.localtime).format('MMMM Do YYYY, h:mm a'));
-        setWeatherData(response.data);
+
+        const location = response.data.location.name + ", " + response.data.location.region + ", " + response.data.location.country;
+        const temp = response.data.current.temp_f + "°F";
+        const date = moment(response.data.location.localtime).format('MMMM Do YYYY, h:mm a');
+
+        //console.log("Location: " + response.data.location.name + ", " + response.data.location.region + ", " + response.data.location.country)
+        //console.log("Temp: " + response.data.current.temp_f + "°F");
+        //console.log("Local time: " + moment(response.data.location.localtime).format('MMMM Do YYYY, h:mm a'));
+
+        axios.post('http://localhost:8080/add', {location: location, temp: temp, date: date});
+        axios.post('http://localhost:8080/show')
+        .then((response) => {
+          console.log("show response: " + response.data);
+        })
       })
       .catch((error) => {
         console.error('Error fetching data:', error);
-        setWeatherData(null);
       });
   };
 
